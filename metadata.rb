0e52563cc139fb8d63b696e918f2fd473a577ac4
pseudomuto <david.muto@gmail.com>
2014-01-12 00:48:25 -0500

    version bump for metadata.json

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ license          'Apache 2.0'
 description      'Installs/Configures git from ppa'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
-version          '0.1.0'
+version          '0.1.1'
 supports         'ubuntu'
 depends          'apt'
