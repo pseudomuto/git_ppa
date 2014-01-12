a80b6694246bd606ad63756d65ab4104043292c4
pseudomuto <david.muto@gmail.com>
2014-01-12 00:07:55 -0500

    adding travis configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,6 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
-
   config.order = 'random'
   config.platform = 'ubuntu'
   config.version = '12.04'
