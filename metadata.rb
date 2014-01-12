b6b2ec2a08fea0ede6ca877f60f910dcc4abfd0b
pseudomuto <david.muto@gmail.com>
2014-01-12 00:53:42 -0500

    version bump for opscode community release

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,12 @@
+name             'git_ppa'
 maintainer       'pseudomuto'
 maintainer_email 'david.muto@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures git from ppa'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
-version          '0.1.1'
+recipe           'git_ppa', 'Installs the latest stable build of git from ppa'
+
+version          '0.1.2'
 supports         'ubuntu'
 depends          'apt'
